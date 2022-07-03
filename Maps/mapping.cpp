847cbfad0ca5e15294edf151493f90fe32408f3d
aman007-cpu <amankaint7@gmail.com>
2022-07-03 23:15:22 +0530

    map

---
+++ Maps/mapping.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <map>
+using namespace std;
+
+int main(){
+ map<int ,string> mp;
+ mp[1] = "2";
+ mp[5] = "1";
+ mp[3] = "3";
+ mp.insert({4,"4"});
+ map<int,int>m;
+ m.insert({9,10});
+ for(auto &i: mp){
+    cout<<i.first<<" "<<i.second<<endl;
+ }
+
+    return 0;
+}
