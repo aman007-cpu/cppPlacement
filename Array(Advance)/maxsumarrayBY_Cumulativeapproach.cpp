d26b7ad942b19fcec3d9bbf90cad3a663e76438c
aman007-cpu <amankaint7@gmail.com>
2022-07-03 13:09:15 +0530

    done

---
+++ Array(Advance)/maxsumarrayBY_Cumulativeapproach.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+
+int main(){
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0;i<n;i++){
+    cin>>arr[i];
+ }
+  int currsum[n+1];
+  currsum[0] = 0;
+  for(int i=1;i<=n;i++){
+    currsum = currsum[i-1] + arr[i-1];
+  }
+
+  int maxsum = INT_MIN;
+  for(int i=1;i<=n;i++){
+    int sum=0;
+    for(int j=0;j<i;j++){
+        sum = currsum[i] - currsum[j];
+        maxsum = max(maxsum,sum);
+    }
+  }
+  cout<<maxsum;
+ return 0;
+
+}
