abb2d0c28762ad967005a6f78d7d37af292c0975
aman007-cpu <amankaint7@gmail.com>
2022-07-07 17:50:00 +0530

    done

---
+++ Strings/binary.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int dp0[n+1];
+    int dp1[n+1];
+    dp0[1] = 1;
+    dp1[1] =  1;
+    for(int i=2;i<=n;i++){
+        dp1[i] = dp1[i-1] + dp0[i-1];
+        dp0[i] = dp1[i-1]; 
+    }
+
+cout<<(dp0[n] + dp1[n]);
+}
