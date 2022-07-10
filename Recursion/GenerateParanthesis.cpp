66936d28073fa61fee2d5660f86b2edded438e74
aman007-cpu <amankaint7@gmail.com>
2022-07-10 20:21:12 +0530

    Backtracking

---
+++ Recursion/GenerateParanthesis.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+vector<string> valid;
+
+void generate(string &s,int open ,int close){
+    if(open==0&&close==0){
+        valid.push_back(s);
+         return;
+    }
+    if(open>0){
+        s.push_back('(');
+        generate(s,open-1,close);
+        s.pop_back();
+    }
+    if(close>0){
+        if(open<close){
+            s.push_back(')');
+            generate(s,open,close-1);
+            s.pop_back();
+        }
+    }
+}
+
+int main(){
+ string s="";
+ int n;
+ cin>>n;
+ generate(s,n,n);
+ for(auto &t: valid){
+    cout<<t<<endl;
+ }
+
+}
