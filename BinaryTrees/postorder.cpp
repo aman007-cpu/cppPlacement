e3e2a826478720c9b858f7bbed6d866270b54d69
aman007-cpu <amankaint7@gmail.com>
2022-07-12 12:38:15 +0530

    good

--- BinaryTrees/postorder.cpp
+++ BinaryTrees/postorder.cpp
@@ -20,6 +20,43 @@ void postorder(struct node*root){
     postorder(root->right);
     cout<<root->data<<" ";
 }
+
+int search(int inorder,int start,int end,int val){
+    for(int i=start,i<=end;i++){
+        if(inorder[i]==val){
+            return i;
+        }
+    }
+    return -1;
+}
+node * buildtree(int post[], int inorder[],in start,int end){
+    static int idx = 4;
+    if(start>end){
+        return nullptr;
+    }
+    int val = post[x];
+    idx--;
+    node cur = new node(val);
+    if(start==end){
+        return cur;
+    }
+   int pos = search(inorder,star,end,val);
+   cur->right = buildtree(pos,inorder,pos+1,end);
+   cur->left = buildtree(pos,inorder,start,pos-1);
+   return curr;
+
+
+}
+
+void inorderprint(node * root){
+    if(root==nullptr){
+        return ;
+    }
+    inorderprint(root->left);
+    cout<<root->data;
+    inorderprint(root->right);
+}
+
 int main(){
   struct node* root = new node(1);
   root->left  = new node(2);
