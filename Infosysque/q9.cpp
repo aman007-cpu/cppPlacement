f00514f4b0ac2f361d367e6b4a0f9f171ac6a8d5
aman007-cpu <amankaint7@gmail.com>
2022-07-15 17:51:50 +0530

    d

---
+++ Infosysque/q9.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+const mod = 10000
+int main(){
+    int n,k;
+    cin>>n>>k;
+
+    int ctr=0;
+    int arr[n];
+    l=1;
+    for(int i=0;i<n;i++){
+        if(l>n){
+            break;
+        }
+        arr[i] = l;
+        
+    }
+
+    int ar[k];
+    for(int i=0;i<k;i++){
+        for(int j=1;j<=n;j++){
+           
+        }
+    }
+
+
+
+}
