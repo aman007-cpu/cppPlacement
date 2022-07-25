5f2069e80e2d4b7e1a53ae1dac191925c2b538c3
Aman Kaintura <amankaint7@gmail.com>
2022-07-25 23:36:02 +0530

    p

---
+++ Pointers/first.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//   int i = 9;
+//     int *ptr = &i;
+//   cout<<i<<endl;
+//   (*ptr)++;
+//   int *q = ptr;
+//   cout<<q<<endl;
+//   cout<<i;
+int arr[10];
+
+ int *p = &arr[0];
+cout<<p<<endl;
+ p = p+1;
+ cout<<p;
+
+
+}
